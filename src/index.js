82c7fe955613d1ecfb549fb2e2e14cdd7da1578b
Asaf Alima <asaf.alima@gmail.com>
2018-06-17 19:26:35 +0300

    Update users and channels tables on duplicate

--- src/index.js
+++ src/index.js
@@ -193,64 +193,60 @@
  */
 
 var mysql = require('mysql');
+var SQL = require('sql-template-strings');
 
 module.exports = function (config) {
   if (!config || !Object.keys(config).length) {
     throw new Error('Need to provide MySQL connection information.');
   }
 
-
   var get = function (tableName, translator) {
     return function (id, callback) {
       var connection = mysql.createConnection(config);
-      connection.connect();
-      connection.query('SELECT * from ' + tableName + ' where id = ?', [id], function (err, rows, fields) {
-        callback(err, translator(rows[0]));
-      });
-      connection.end();
+      try {
+        connection.connect();
+        connection.query(SQL`SELECT * from `.append(tableName).append(SQL` where id = ${id}`), function (err, rows) {
+            callback(err, translator(rows[0]));
+        });
+      } finally {
+        connection.end();
+      }
     };
   };
 
   var saveUser = function (tableName) {
     return function (data, callback) {
-      var dataToSave = {
-        id: data.id,
-        access_token: data.access_token,
-        scopes: JSON.stringify(data.scopes),
-        team_id: data.team_id,
-        user: data.user
-      };
-      save('INSERT into ' + tableName + ' SET ?', dataToSave, callback);
+      var id = data.id;
+      var access_token = data.access_token;
+      var scopes = JSON.stringify(data.scopes);
+      var team_id = data.team_id;
+      var user = data.user;
+
+      save(SQL`INSERT into `.append(tableName).append(SQL` (id, access_token, scopes, team_id, user)`)
+        .append(SQL`VALUES (${id}, ${access_token}, ${scopes}, ${team_id}, ${user})`)
+        .append(SQL`ON DUPLICATE KEY UPDATE id=${id}, access_token=${access_token}, scopes=${scopes}, team_id=${team_id}, user=${user}`),
+        callback);
     };
   };
 
   var saveTeam = function (tableName) {
     return function (data, callback) {
-      var getTeam = get('botkit_team', dbToTeamJson);
-      var dataToSave = {
-        id: data.id,
-        createdBy: data.createdBy,
-        name: data.name,
-        url: data.url,
-        token: data.token,
-        bot: JSON.stringify(data.bot)
-      };
-      getTeam(dataToSave.id, function(err, teamData) {
-        if (!teamData) {
-          save('INSERT into ' + tableName + ' SET ?', dataToSave, callback);
-        } else {
-          var updateSql = 'UPDATE botkit_team SET createdBy = ?, name = ?, url = ?, token = ?, bot = ? WHERE id = ?';
-          var updateSqlData = [dataToSave.createdBy, dataToSave.name, dataToSave.url, dataToSave.token, dataToSave.bot, dataToSave.id];
+      var id = data.id;
+      var createdBy = data.createdBy;
+      var name = data.name;
+      var url = data.url;
+      var token = data.token;
+      var bot = JSON.stringify(data.bot);
 
-          save(updateSql, updateSqlData, callback);
-        }
-      });
+      save(SQL`INSERT into `.append(tableName).append(SQL` (id, createdBy, name, url, token, bot)`)
+          .append(SQL`VALUES (${id}, ${createdBy}, ${name}, ${url}, ${token}, ${bot})`)
+          .append(SQL`ON DUPLICATE KEY UPDATE createdBy = ${createdBy}, name = ${name}, url = ${url}, token = ${token}, bot = ${bot}`),
+          callback);
     };
   };
 
   var saveChannel = function (tableName) {
     return function (data, callback) {
-      var dataToSave = {id: data.id};
       var keys = Object.keys(data);
       var json = {};
       for (var i = 0; i < keys.length; i++) {
@@ -259,33 +255,41 @@ module.exports = function (config) {
           json[key] = data[key];
         }
       }
-      dataToSave.json = JSON.stringify(json);
 
-      save('INSERT into ' + tableName + ' SET ?', dataToSave, callback);
+      var stringifiedJson = JSON.stringify(json);
+      save(SQL`INSERT into `.append(tableName).append(SQL` (id, json)`)
+          .append(SQL`VALUES (${data.id}, ${stringifiedJson})`)
+          .append(SQL`ON DUPLICATE KEY UPDATE json=${stringifiedJson}`), callback);
     };
   };
 
-  var save = function (statement, dataToSave, callback) {
+  var save = function (statement, callback) {
     var connection = mysql.createConnection(config);
-    connection.connect();
-    connection.query(statement, dataToSave, function (err, rows, fields) {
-      callback(err);
-    });
-    connection.end();
+      try {
+        connection.connect();
+        connection.query(statement, function (err) {
+            callback(err);
+        });
+      } finally {
+        connection.end();
+      }
   };
 
   var all = function (tableName, translator) {
     return function (callback) {
       var connection = mysql.createConnection(config);
-      connection.connect();
-      connection.query('SELECT * from ' + tableName, function (err, rows, fields) {
-        var translatedData = [];
-        for (var i = 0; i < rows.length; i++) {
-          translatedData.push(translator(rows[i]))
-        }
-        callback(err, rows);
-      });
-      connection.end();
+      try {
+        connection.connect();
+        connection.query(SQL`SELECT * from `.append(tableName), function (err, rows) {
+            var translatedData = [];
+            for (var i = 0; i < rows.length; i++) {
+                translatedData.push(translator(rows[i]))
+            }
+            callback(err, translatedData);
+        });
+      } finally {
+        connection.end();
+      }
     };
   };
 
--- test/index.js
+++ test/index.js
@@ -1,19 +1,41 @@
 "use strict";
 var chai = require('chai');
-var mysql = require('mysql');
 var expect = chai.expect;
+var Storage = require('../src/index.js');
 
+function copy(data) {
+  return JSON.parse(JSON.stringify(data));
+}
 
-var config = {host: 'localhost', user: 'root', password: '', database: 'my_schema'};
-var globalId = "-12345678";
+function testDataRetrievingAndManipulation(scope, id, initialData, keyToUpdate, done) {
+  var storage = Storage({
+    host: 'localhost',
+    user: 'root',
+    password: 'p@ssw0rd',
+    database: 'tests'
+  });
+
+  storage[scope].save(initialData, function (err) {
+    expect(err).to.be.null;
+    storage[scope].get(id, function (err, actualData) {
+      expect(actualData).to.deep.equal(initialData);
+
+      var newData = copy(initialData);
+      newData[keyToUpdate] = 'andNewValue';
+      storage[scope].save(newData, function (err) {
+        expect(err).to.be.null;
+        storage[scope].all(function (err, data) {
+          expect(err).to.be.null;
+          expect(data.length).to.be.equal(1);
+          expect(data[0]).to.deep.equal(newData);
+          done();
+        })
+      });
+    })
+  });
+}
 
 describe('Botkit MySQL Storage', function() {
-
-  var Storage;
-
-  beforeEach(function() {
-    Storage = require('../src/index.js');
-  });
 
   describe('module configuration', function () {
     it('should throw an error when configuration is null', function () {
@@ -39,5 +61,47 @@ describe('Botkit MySQL Storage', function() {
       expect(Storage({host:'localhost'}).users.save).to.not.equal(undefined);
       expect(Storage({host:'localhost'}).users.all).to.not.equal(undefined);
     });
+  });
+
+  describe('Data retrieving and manipulation', function () {
+
+    it('channel data should be created, retrieved, updated and deleted', function(done) {
+        var id = 'channelId';
+        var initialData = {
+            id: id,
+            testField: 'testValue',
+            testNumericField: 99
+        };
+
+        testDataRetrievingAndManipulation('channels', id, initialData, 'testField', done);
+    });
+
+    it('user data should be created, retrieved, updated and deleted', function(done) {
+        var id = 'userId';
+        var initialData = {
+            id: id,
+            access_token: 'some_access_token',
+            scopes: '[]',
+            team_id: 'teamId',
+            user: 'username'
+        };
+
+        testDataRetrievingAndManipulation('users', id, initialData, 'access_token', done);
+    });
+
+    it('team data should be created, retrieved, updated and deleted', function(done) {
+        var id = 'teamId';
+        var initialData = {
+            id: id,
+            createdBy: 'userId',
+            name: 'testName',
+            url: 'teamUrl',
+            token: 'someToken',
+            bot: 'myBot',
+        };
+
+        testDataRetrievingAndManipulation('teams', id, initialData, 'token', done);
+    });
+
   });
 });
